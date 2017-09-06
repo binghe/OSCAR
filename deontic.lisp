7200ff60087334f8b57e87917b6e2235cff9e615
Chun Tian (binghe) <binghe.lisp@gmail.com>
2017-09-06 23:52:23 +0200

    Use rules2.lisp for deontic rules

--- deontic.lisp
+++
@@ -1,5 +0,0 @@
-;; Deontic operators and their reasoning rules for Legal Reasoning
-;; Author: Chun Tian (binghe)
-
-(in-package :oscar)
-
--- rules2.lisp
+++ rules2.lisp
@@ -9,16 +9,16 @@
         "-p"
     :variables  p q -p)
 
-; Suppositional rules
+;;; Suppositional rules
 
 (def-backwards-reason contra-conditionalization
     :conclusions "(p -> q)"
     :condition (or (and (not (u-genp p))
-                                   (not (conjunctionp (quantifiers-matrix p)))
-                                   (e-genp q))
-                           (and (literal p)
-                                    (literal q)
-                                    (or (negationp p) (negationp q))))
+			(not (conjunctionp (quantifiers-matrix p)))
+			(e-genp q))
+		   (and (literal p)
+			(literal q)
+			(or (negationp p) (negationp q))))
     :backwards-premises "~p"
     :discharge "~q"
     :variables  p q)
@@ -26,18 +26,21 @@
 (def-backwards-reason conditionalization
     :conclusions "(p -> q)"
     :condition (or (u-genp p)
-                           (and (literal p) (not (e-genp q)))
-                           (conjunctionp (quantifiers-matrix p))
-                           (not (e-genp q)))
+		   (and (literal p) (not (e-genp q)))
+		   (conjunctionp (quantifiers-matrix p))
+		   (not (e-genp q)))
     :backwards-premises "q"
     :discharge "p"
     :variables  p q)
 
-(setf *backwards-logical-reasons*
-         (list adjunction neg-intro i-neg-disj i-neg-condit i-neg-bicondit
-                 bicondit-intro disj-cond i-DM conditionalization
-                 i-neg-ug i-neg-eg EG UG
-                 disj-cond-2
-                 contra-conditionalization
-                ; vacuous-condition
-                 ))
+(setq *backwards-logical-reasons*
+      (list adjunction neg-intro i-neg-disj i-neg-condit i-neg-bicondit
+	    bicondit-intro disj-cond i-DM conditionalization
+	    i-neg-ug i-neg-eg EG UG
+	    disj-cond-2
+	    contra-conditionalization
+	    ;; vacuous-condition
+	    ))
+
+;;; Rules of deontic operators for legal reasoning.
+
